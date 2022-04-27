4cb59e1dade248fafc642e1ee89c5d4caefa075c
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-04-27 10:34:17 +0200

    Add linear-regression model

---
+++ classifications/linear-regression.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Sequential
+
+xs = np.arange(0.0, 150.0, 0.1)
+xs = tf.convert_to_tensor(xs)
+
+ys = [x + 6.0 for x in xs]
+ys = tf.convert_to_tensor(ys)
+
+
+model = Sequential()
+model.add(Input(shape=(1,)))
+model.add(Dense(1))
+
+model.compile(loss='mae', optimizer='adam', metrics=['mae'])
+
+model.fit(x=xs, y=ys, epochs=200)
+
+print('Prediction is: ', model.predict([10.0]))
