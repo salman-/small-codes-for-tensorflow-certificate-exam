997bdf3b48310ce2b2d1b8e5b1168964a591362b
Salman Lashkarara <salman.lashkarara@diconium.com>
2022-04-27 10:49:51 +0200

    update dataset creation

--- classifications/linear-regression.py
+++ classifications/linear-regression.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Sequential
 
+#xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], dtype = float)
 xs = np.arange(0.0, 150.0, 0.1)
-xs = tf.convert_to_tensor(xs)
 
 ys = [x + 6.0 for x in xs]
 ys = tf.convert_to_tensor(ys)
